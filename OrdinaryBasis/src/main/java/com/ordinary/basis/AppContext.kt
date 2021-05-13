43b62bb840842a1a8eaafb6b594af13b1bdd4189
LovewLever <lovewlever@outlook.com>
2021-05-13 15:56:31 +0800

    Retrofit

--- OrdinaryBasis/src/main/java/com/ordinary/basis/AppContext.kt
+++ OrdinaryBasis/src/main/java/com/ordinary/basis/AppContext.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import android.content.pm.ApplicationInfo
 import com.ordinary.basis.other.TimberCloseTree
 import com.ordinary.basis.retrofit.RetrofitCommon
+import com.ordinary.basis.retrofit.converter.CustomGsonConverterFactory
+import okhttp3.Interceptor
+import retrofit2.CallAdapter
+import retrofit2.Converter
 import timber.log.Timber
 
 object AppContext {
 
     lateinit var application: Application
 
-    fun initialization(application: Application, basisUrl: String) {
+    fun initialization(application: Application) {
         this.application = application
         // 日志
         if ((application.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
@@ -18,6 +22,5 @@ object AppContext {
         } else {
             Timber.plant(TimberCloseTree())
         }
-        RetrofitCommon.initialization(basisUrl)
     }
 }
